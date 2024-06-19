aa1b7b01a036cf0353d18298f3ebe142bf74692a
SILMARIL\DanielBogdan <danieel.bogdan@gmail.com>
2024-06-19 23:37:08 +0300

    - setup vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import '@/assets/index.scss';
+import 'vuetify/styles';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createVuetify } from 'vuetify';
+
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 import App from './App.vue';
 import router from '@/app/router';
 
 const app = createApp(App);
 
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+
 app.use(createPinia());
 app.use(router);
+app.use(vuetify);
 
 app.mount('#app');
