ac29cb8fbab41db8f535bef9694d5147393a5edd
SILMARIL\DanielBogdan <danieel.bogdan@gmail.com>
2024-06-22 17:33:23 +0300

    - add dialog for edit

---
+++ src/components/dialogs/EditAssetDialog.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-dialog
+        @update:modelValue="hideEditDialog"
+        :model-value="props.showEditDialog"
+        max-width="480">
+        <v-card title="Edit Asset">
+            <div class="pa-4 text-center">{{ props.asset.title }}</div>
+            <v-card-actions>
+                <v-btn text="Cancel" variant="text" @click="hideEditDialog"></v-btn>
+                <v-btn text="Save" :disabled="true" color="primary" variant="text"></v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { onMounted, type PropType } from 'vue';
+import type { IAssetItem } from '@/contracts/IAsset';
+const emit = defineEmits(['editItem', 'hideEditDialog']);
+
+const props = defineProps({
+    showEditDialog: {
+        type: Boolean,
+        required: true,
+    },
+    asset: {
+        type: Object as PropType<IAssetItem>,
+        required: true,
+    },
+});
+
+const hideEditDialog = () => {
+    emit('hideEditDialog');
+};
+
+onMounted(() => {
+    console.log(props.asset);
+});
+</script>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -12,14 +12,13 @@
             <AddressCell
                 :street="item.street"
                 :street-number="item.street_number"
-                :postal-code="item.postal_code">
-            </AddressCell>
+                :postal-code="item.postal_code" />
         </template>
         <template v-slot:item.created_at="{ item }">
-            <DateCell :date="item.available_from"></DateCell>
+            <DateCell :date="new Date(item.available_from)" />
         </template>
         <template v-slot:item.updated_at="{ item }">
-            <DateCell :date="item.updated_at"></DateCell>
+            <DateCell :date="new Date(item.updated_at)" />
         </template>
         <template v-slot:bottom>
             <div class="d-flex flex-row-reverse mt-2">
@@ -27,59 +26,51 @@
                     <v-pagination
                         :model-value="currentPage"
                         @update:modelValue="onPageChange"
-                        :length="pageCount">
-                    </v-pagination>
+                        :length="pageCount" />
                 </div>
             </div>
         </template>
+        <template v-slot:item.actions="{ item }">
+            <v-icon class="me-2" @click="showEditDialog(item)">edit</v-icon>
+        </template>
     </v-data-table-server>
+    <EditAssetDialog
+        v-if="assetToEdit"
+        :show-edit-dialog="editDialogIsVisible"
+        :asset="assetToEdit"
+        @hide-edit-dialog="hideEditDialog" />
 </template>
 
 <script setup lang="ts">
-import type { IPagination } from '@/contracts/IPagination';
-import { useAssetsStore } from '@/stores/asset/asset_store';
-import { storeToRefs } from 'pinia';
 import { computed, ref } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useAssetsStore } from '@/stores/asset/asset_store';
+import type { IPagination } from '@/contracts/IPagination';
+import type { IAssetItem } from '@/contracts/IAsset';
 import AddressCell from '@/components/grid/AddressCell.vue';
 import DateCell from '@/components/grid/DateCell.vue';
+import EditAssetDialog from '@/components/dialogs/EditAssetDialog.vue';
 
 const { fetchAssets } = useAssetsStore();
 const { assets, meta, state } = storeToRefs(useAssetsStore());
+
 const pageCount = computed(() => meta.value?.last_page);
 const currentPage = computed(() => meta.value?.current_page);
 const totalItems = computed(() => meta.value?.total ?? 0);
 const loading = computed(() => state.value === 'LOADING');
+
 const gridHeight = 920;
 const headers = computed(() => [
-    {
-        title: 'Title',
-        key: 'title',
-    },
-    {
-        title: 'Type',
-        key: 'type.name',
-    },
-    {
-        title: 'Size',
-        key: 'size',
-    },
-    {
-        title: 'Address',
-        key: 'address',
-    },
-    {
-        title: 'Description',
-        key: 'description',
-    },
-    {
-        title: 'Created',
-        key: 'created_at',
-    },
-    {
-        title: 'Updated',
-        key: 'updated_at',
-    },
+    { title: 'Title', key: 'title' },
+    { title: 'Type', key: 'type.name' },
+    { title: 'Size', key: 'size' },
+    { title: 'Address', key: 'address' },
+    { title: 'Description', key: 'description' },
+    { title: 'Created', key: 'created_at' },
+    { title: 'Updated', key: 'updated_at' },
+    { title: 'Actions', key: 'actions' },
 ]);
+
 const pagination = ref<IPagination>({
     itemsPerPage: 30,
     page: 1,
@@ -88,5 +79,18 @@ const pagination = ref<IPagination>({
 const onPageChange = (event: number) => {
     pagination.value.page = event;
     fetchAssets(pagination.value);
+};
+
+const assetToEdit = ref<IAssetItem | null>(null);
+const editDialogIsVisible = ref(false);
+
+const showEditDialog = (asset: IAssetItem) => {
+    assetToEdit.value = asset;
+    editDialogIsVisible.value = true;
+};
+
+const hideEditDialog = () => {
+    assetToEdit.value = null;
+    editDialogIsVisible.value = false;
 };
 </script>
